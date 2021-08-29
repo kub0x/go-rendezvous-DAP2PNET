fb03b7d88f24e4e34a95aac4f582c1bd5a9fbd92
Borja Gomez <borja.gomez@ironchip.com>
2021-08-29 17:54:28 +0200

    changes for forwarding tls client cert via traefik on mutual TLS

--- middlewares/Authorization.go
+++ middlewares/Authorization.go
@@ -5,6 +5,9 @@ import "github.com/gin-gonic/gin"
 func SetPeerIdentity() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		identity := c.GetHeader("Authorization")
+		tlsHeader := c.GetHeader("X-Forwarded-Tls-Client-Cert-Info")
+		println("TLS HEADER: " + tlsHeader)
+
 		if identity == "" {
 
 		}
